a1150db4beb6fb33ff049f76a4eb155d87848c97
Gene <genewong2016@gmail.com>
2022-12-02 15:17:19 +0800

    Initial commit: Update vue-news-app

---
+++ src/components/Footer.vue
@@ -0,0 +1,6 @@
+<template>
+  <footer
+    class="px-4 py-8 text-sm font-bold text-center text-green-100 bg-green-900">
+    <p class="text-sm tracking-wide">Copyright (c) 2021 SitePoint</p>
+  </footer>
+</template>
